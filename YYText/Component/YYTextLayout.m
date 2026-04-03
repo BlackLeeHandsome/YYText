9e874605bc01f503d7c385bb5de28fb3ae5d486c
lidong <18668094687@163.com>
2026-04-03 17:17:12 +0800

    fix：XCode 26.4 编译报错修复

--- YYText/Component/YYTextLayout.m
+++ YYText/Component/YYTextLayout.m
@@ -1504,18 +1504,36 @@ fail:
     }
     
     [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+        /*
         if (isVertical) {
             position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
         } else {
             position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
         }
+         */
+        /// 👇👇👇下面为修复代码
+        /// 作者意图：距离上边界更近则选择 prev，距离下边界更近则选择 next
+        /// 作者意图：距离左边界更近则选择 prev，距离右边界更近则选择 next
+        if (isVertical) {
+            position = (fabs(left - point.y) < fabs(right - point.y)) ? prev : next;
+        } else {
+            position = (fabs(left - point.x) < fabs(right - point.x)) ? prev : next;
+        }
     }];
     
     [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+        /*
         if (isVertical) {
             position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
         } else {
             position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+        }
+         */
+        /// 👇👇👇下面为修复代码
+        if (isVertical) {
+            position = (fabs(left - point.y) < fabs(right - point.y)) ? prev : next;
+        } else {
+            position = (fabs(left - point.x) < fabs(right - point.x)) ? prev : next;
         }
     }];
     
